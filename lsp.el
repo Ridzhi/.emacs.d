5368f56586d03cfc608c35e13db214780fd66caf
Danila Stivrinsh <ridzhi@livexp.com>
2024-10-04 12:42:44 +0300

    upd

--- lsp.el
+++ lsp.el
@@ -1,14 +1,15 @@
 (use-package lsp-mode
 	:ensure t
+	:config
+	(setq lsp-inlay-hint-enable t)
+	(setq lsp-rust-analyzer-binding-mode-hints t)
 	:hook
 	(rust-mode . lsp)
-	:custom
-	(setq lsp-rust-analyzer-binding-mode-hints t)
 	:commands lsp)
 
 (use-package company
 	:ensure t
-	:custom
-	(company-idle-delay 0.5))
+	:config
+	(setq company-idle-delay 0.5))
 
 (use-package flycheck :ensure t)
