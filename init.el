de6002c10eba492ee8752c77f076a7a02893a833
Danila Stivrinsh <ridzhi@livexp.com>
2024-10-05 17:02:18 +0300

    upd

--- init.el
+++ init.el
@@ -1,11 +1,7 @@
-(setq inhibit-startup-screen t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-
-(load "~/.emacs.d/packages.el")
-(load "~/.emacs.d/rust.el")
-(load "~/.emacs.d/lsp.el")
-
+(use-package package
+	:config
+	(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+	(package-initialize))
 
 
 (custom-set-variables
@@ -20,3 +16,40 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq inhibit-startup-screen t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+
+;; Themes
+(use-package zenburn-theme
+	:ensure t
+	:config
+	(load-theme 'zenburn t))
+
+;; Programming
+(use-package lsp-mode
+	:ensure t
+	:config
+	(setq lsp-inlay-hint-enable t)
+	(setq lsp-rust-analyzer-binding-mode-hints t)
+	:hook
+	(rust-mode . lsp)
+	:commands lsp)
+
+(use-package company
+	:ensure t
+	:config
+	(setq company-idle-delay 0.5))
+
+(use-package flycheck :ensure t)
+
+
+(use-package rust-mode
+	:ensure t
+	:init
+	(setq rust-mode-treesitter-derive t)
+	:custom
+	(rust-format-on-save t))
+
