c1490a4834597798affe25a30c4ed0f550788a54
Danila Stivrinsh <ridzhi@livexp.com>
2024-10-05 17:25:46 +0300

    upd

--- init.el
+++ init.el
@@ -9,13 +9,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-line-numbers t)
  '(package-selected-packages '(rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "nil" :family "JetBrains Mono")))))
 
 (setq inhibit-startup-screen t)
 (scroll-bar-mode -1)
--- rust.el
+++
@@ -1,6 +0,0 @@
-(use-package rust-mode
-	:ensure t
-	:init
-	(setq rust-mode-treesitter-derive t)
-	:custom
-	(rust-format-on-save t))
